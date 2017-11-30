3bc16464d1a9d1ddc8270f423a2d0a0ac6c3e2ca
BrandonDG <brandon.gillespie08@gmail.com>
2017-11-30 12:09:54 -0800

    Done basic framework of the UI, it's not pretty, but it will be until I start adding functionality

--- app/src/main/java/com/brandon/tasktrack/SettingsActivity.java
+++ app/src/main/java/com/brandon/tasktrack/SettingsActivity.java
@@ -1,8 +1,11 @@
 package com.brandon.tasktrack;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -14,5 +17,36 @@ public class SettingsActivity extends AppCompatActivity {
 
     public void settingsBackToTask(final View v) {
         finish();
+    }
+
+    public void toCreateTaskTypeModal(final View v) {
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_tasktypeedit);
+
+        Button cancelButton = (Button) dialog.findViewById(R.id.newTaskTypeModal_cancelbutton);
+        //Button toTypesButton = (Button) dialog.findViewById(R.id.settingsModal_totypesbutton);
+
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.copyFrom(dialog.getWindow().getAttributes());
+        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                dialog.dismiss();
+            }
+        });
+
+        /*
+        toTypesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                toSettings();
+            }
+        }); */
+
+        dialog.show();
+        dialog.getWindow().setAttributes(lp);
     }
 }
--- app/src/main/java/com/brandon/tasktrack/TaskActivity.java
+++ app/src/main/java/com/brandon/tasktrack/TaskActivity.java
@@ -55,8 +55,35 @@ public class TaskActivity extends AppCompatActivity {
         startActivity(i);
     }
 
-    public void addTask(final View v) {
+    public void toAddTaskModal(final View v) {
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_newtask);
 
+        Button cancelButton = (Button) dialog.findViewById(R.id.newTaskModal_cancelbutton);
+        //Button toTypesButton = (Button) dialog.findViewById(R.id.settingsModal_totypesbutton);
+
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.copyFrom(dialog.getWindow().getAttributes());
+        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                dialog.dismiss();
+            }
+        });
+
+        /*
+        toTypesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                toSettings();
+            }
+        }); */
+
+        dialog.show();
+        dialog.getWindow().setAttributes(lp);
     }
 
     public void toSettings() {
